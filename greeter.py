ed35f7e01551c093ecd940f1dfc306720399a47b
Klemen Kotnik <Klemen.Kotnik@endava.com>
2025-02-14 15:59:14 +0100

    Add parametrization

--- greeter.py
+++ greeter.py
@@ -1,6 +1,9 @@
 def main():
-    student = input("Please enter your name: ")
+    print("######################\nSTUDENT ENROLLMENT FORM\N##############")
 
-    print(f"Dear {student}, we are happy to have you in the Data Science track!")
+    student = input("Please enter your name: ")
+    uni = input("Please enter your University name: ")
+    track = input("Please enter you study programme: ")
+    print(f"Dear {student}, we are happy to have you in the {track} track at the {uni}!")
 
 main()
